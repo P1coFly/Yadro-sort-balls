947349c629cf21aa76ba7e7fb42688e3a334b884
Artem <artem3m@mail.ru>
2025-05-04 18:04:54 +0300

    add some testcase

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -20,17 +20,53 @@ func TestСanSortBalls(t *testing.T) {
 			Matrix: [][]int{{10, 20, 30}, {1, 1, 1}, {0, 0, 1}},
 			Answer: no,
 		},
+		{
+			Matrix: [][]int{{10, 20}, {20, 10}},
+			Answer: yes,
+		},
+		{
+			Matrix: [][]int{{1, 0}, {0, 1}},
+			Answer: yes,
+		},
+		{
+			Matrix: [][]int{{1, 1}, {1, 1}},
+			Answer: yes,
+		},
+		{
+			Matrix: [][]int{{1, 1}, {0, 0}},
+			Answer: no,
+		},
 	}
 
 	for caseNum, item := range cases {
 
 		res := canSortBalls(item.Matrix)
-
 		if res != item.Answer {
 			t.Errorf("[%d] wrong Answer: got %s, expected %s",
 				caseNum, res, item.Answer)
 		}
+	}
+}
 
+func TestСanSortBallsWrongSizeMatrix(t *testing.T) {
+
+	cases := []TestCaseSortBalls{
+		{
+			Matrix: [][]int{{1, 2}, {2, 1}, {0, 0}},
+			Answer: no,
+		},
+		{
+			Matrix: [][]int{{10, 20, 0}, {20, 10, 0}},
+			Answer: no,
+		},
 	}
 
+	for caseNum, item := range cases {
+
+		res := canSortBalls(item.Matrix)
+		if res != item.Answer {
+			t.Errorf("[%d] wrong Answer: got %s, expected %s",
+				caseNum, res, item.Answer)
+		}
+	}
 }
